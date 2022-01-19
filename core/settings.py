e8b9a427b56fb9bc2be000a5e7a9e7bedd11d082
Ahnas <manuahnas@gmail.com>
2022-01-20 01:23:57 +0530

    Adding User , Teacher , Student Models

--- core/settings.py
+++ core/settings.py
@@ -130,3 +130,4 @@ STATIC_ROOT = BASE_DIR / 'assets'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'student.User'
--- student/models.py
+++ student/models.py
@@ -1,3 +1,56 @@
+from pyexpat import model
 from django.db import models
 
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        """Create Save a User"""
+        if not email:
+            raise ValueError('User must have a Email')
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        if user:
+            return user
+
+    def create_superuser(self, email, password):
+        """Create and Save a super User"""
+        user = self.model(email=email)
+        user.set_password(password)
+        user.save(using=self.db)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+
+class User(AbstractBaseUser):
+    """"Custom Model"""
+    email = models.EmailField(max_length=225, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return str(self.email)
+
+
+class Teacher(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    name=models.CharField(max_length=225)
+    department=models.CharField(max_length=225)
+    phone=models.CharField(max_length=10)
+
+class Student(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    name=models.CharField(max_length=225)
+    dob=models.CharField(max_length=50)
+    phone=models.CharField(max_length=10)
